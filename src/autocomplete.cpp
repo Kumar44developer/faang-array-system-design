a8dc5961736d9a3d6f391b8c2eafe7bf57aa2c3b
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-06 14:34:23 +0530

    Implemented prefix search in AutoComplete using binary search

--- src/autocomplete.cpp
+++ src/autocomplete.cpp
@@ -6,3 +6,13 @@ struct AutoComplete {
         dict = move(words);
         sort(dict.begin(), dict.end());
     }
+   vector<string> suggest(const string& prefix, int limit) const {
+        string lo = prefix;
+        string hi = prefix; hi.push_back(char(127)); // upper bound sentinel
+        auto it1 = lower_bound(dict.begin(), dict.end(), lo);
+        auto it2 = lower_bound(dict.begin(), dict.end(), hi);
+        vector<string> out;
+        for (; it1 != it2 && (int)out.size() < limit; ++it1) out.push_back(*it1);
+        return out;
+    }
+};
