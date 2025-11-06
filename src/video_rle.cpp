2b7ac57f360318d01e0d915fa9212127f8bfb0dc
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-06 14:29:08 +0530

    Implemented row-wise run-length encoding in RLEFrame::encodeRow

--- src/video_rle.cpp
+++ src/video_rle.cpp
@@ -1,1 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct RLEFrame {
+    static vector<pair<int,int>> encodeRow(const vector<int>& row) { 
+        vector<pair<int,int>> out; if (row.empty()) return out;
+        int cur = row[0], cnt = 1;
+        for (size_t i = 1; i < row.size(); ++i) {
+            if (row[i] == cur) ++cnt;
+            else { out.push_back({cur, cnt}); cur = row[i]; cnt = 1; }
+        }
+        out.push_back({cur, cnt});
+        return out;
+    }
 
