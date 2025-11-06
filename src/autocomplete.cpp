eba47edbe1d112a30e85d4be99b4fd714ad8b406
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-06 14:33:39 +0530

    Implemented AutoComplete with sorted word dictionary

--- src/autocomplete.cpp
+++ src/autocomplete.cpp
@@ -1,1 +1,8 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+struct AutoComplete {
+    vector<string> dict;
+    explicit AutoComplete(vector<string> words) {
+        dict = move(words);
+        sort(dict.begin(), dict.end());
+    }
